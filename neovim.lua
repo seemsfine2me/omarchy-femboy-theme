4bb6fd62de7ba9d7c49512727efaa2d46c76f2e0
seemsfine2me <earth.from.mars2030@gmail.com>
2025-11-28 22:48:11 -0800

    Change colorscheme from 'radius' to 'femboy'

--- neovim.lua
+++ neovim.lua
@@ -2,15 +2,15 @@ return {
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "radius",
+			colorscheme = "femboy",
 		},
 	},
 	{
-		"lalitmee/radius.nvim",
+		"lalitmee/femboy.nvim",
 		event = { "ColorSchemePre" }, -- if you want to lazy load
 		dependencies = { "tjdevries/colorbuddy.nvim", tag = "v1.0.0" },
 		init = function()
-			require("colorbuddy").colorscheme("radius")
+			require("colorbuddy").colorscheme("femboy")
 		end,
 	},
 }
